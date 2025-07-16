2bf523ed91697cedf520244e9877b482d3a27a31
Felipe B. <felipefantinn@outlook.com>
2025-07-15 21:57:32 -0300

    Remove button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Box from './Components/Box';
 import Divider from './Components/Divider'; 
 import Steps from './Components/Steps';
-import styles from './App.module.css';
 
 function App() {
   return (
@@ -15,17 +14,13 @@ function App() {
       ]} />
       <Divider />
 
-      <form style={{display:"flex", flexDirection:"column", gap:24,marginTop:"24px"}}>
+      <form style={{display:"flex", flexDirection:"column", gap:24, marginTop:24}}>
         <input/>
         <input/>
         <input/>
       </form>
       <button className='button-enviar'>Enviar</button>
     </Box>
-
-    <div className={styles.modules}>
-      Meu componente
-    </div>
   </div>
   );
 }
